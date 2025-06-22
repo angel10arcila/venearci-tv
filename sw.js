2acbd00f280b0f5ef51085b6f03165506a410940
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-06-22 13:56:10 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Simple offline cache para PWA
+const CACHE_NAME = 'angel10arcila-maqueta-cache-v1';
+const urlsToCache = [
+  '/', 
+  'index.html',
+  'style.css',
+  'install.js',
+  'manifest.json'
+];
+
+// Instalar SW
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activar SW
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
