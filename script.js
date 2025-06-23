bb81e6dd5278a3f66fa01b0ef0b66e09276e4903
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-06-23 11:15:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const mediaFiles = [
     },
     {
         name: "Televen",
-        url: "https://mediablocks-ve.global.ssl.fastly.net/10255/live/hls/televen/index.m3u8?hdnts=data%3Dip%3D200.93.97.245%2Cid%3DTLV-GUEST%2Cpid%3D255%2Cdid%3DN%2FA%2Csid%3DWBS~acl%3D%2F10255%2Flive%2Fhls%2Fteleven%2F%2A~exp%3D1750637695~hmac%3D48354fa28e685a624a055e0e6ad0be5afc87293fb5e480924313c293f1d16b90",
+        url: "https://mediablocks-ve.global.ssl.fastly.net/10255/live/hls/televen/index.m3u8?hdnts=data%3Dip%3D200.93.97.245%2Cid%3DTLV-GUEST%2Cpid%3D255%2Cdid%3DN%2FA%2Csid%3DWBS~acl%3D%2F10255%2Flive%2Fhls%2Fteleven%2F%2A~exp%3D1750702381~hmac%3D90b249d5af8091b557596b3c1635c6096349abe8bcce260788f223c0ef78d008",
         type: "Tv"
     },
     {
