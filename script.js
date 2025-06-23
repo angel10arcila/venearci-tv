0e0d3234705d774636e8a31fa3cd53e4cd6322ca
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-06-23 18:42:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const mediaFiles = [
     },
     {
         name: "DirecTV",
-        url: "https://cdn15.vivozytv.com/hotflix/event3/index.m3u8?token=80a8a18a9399bc20d3c90605d335d2e3325f5697-16560c5502fdc353a1c86803dd30dfd2-1750715632-1750704832&remote=200.93.97.245",
+        url: "https://cdn12.vivozytv.com/hotflix/dsports/index.m3u8?token=eba54e74c2dd5e23c874772b1d43c41564d47d28-45b22bb9bb32c455139cb137c1e8faf7-1750728980-1750718180&remote=200.93.97.245",
         type: "Tv"
     },
     {
